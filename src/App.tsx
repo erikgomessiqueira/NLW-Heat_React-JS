d74d2e6441cbc13bb539e40ea7bffd323ce47679
Erik-Gomes-Siqueira <erikgomessiqueira.estudante@gmail.com>
2021-10-20 21:53:00 -0300

    Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import styles from "./App.module.scss"
+import { useContext } from "react";
+
+import styles from "./App.module.scss";
+
+import { MessageList } from "./components/MessageList";
+import { LoginBox } from "./components/LoginBox"
+
+import { AuthContext } from "./contexts/auth";
+import { SendMessageForm } from "./components/SendMessageForm";
 
 
 export function App() {
-
+    const {user} = useContext(AuthContext)
   return (
-    <main className={styles.contentWrapper}>
-      <h1>Hello Word</h1>
+    <main className={`${styles.contentWrapper} ${!!user?styles.contentSigned: ''}`}>
+      <MessageList />
+      { !!user? <SendMessageForm /> : <LoginBox /> }
     </main>
   )
 }
---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,25 @@
+import { useContext } from "react"
+import { VscGithubInverted } from "react-icons/vsc"
+import { AuthContext } from "../../contexts/auth"
+
+
+import styles from "./styles.module.scss"
+
+
+export function LoginBox(){
+    const { signInUrl } = useContext(AuthContext)
+
+
+    
+    return(
+        <div className={styles.loginBoxWrapper}>
+            <strong>Entre e compartilhe sua menssagem</strong>
+            <a href={signInUrl} className={styles.singnInWithGithub}>
+                <VscGithubInverted size="24" />
+                Entrar com o GitHub
+            </a>
+
+        </div>
+
+    )
+}
